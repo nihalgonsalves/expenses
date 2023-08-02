2dc18290f2e5568c93e65d1e8657f8c13b933afe
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-08-03 01:17:33 +0200

    chore: disable flip-flopping prettier-tailwindcss

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,12 +3,7 @@ import baseConfig from '@nihalgonsalves/esconfig/.prettierrc.js';
 /** @type {import('prettier').Config} */
 export default {
   ...baseConfig,
-  plugins: [
-    'prettier-plugin-prisma',
-    'prettier-plugin-sql',
-    'prettier-plugin-tailwindcss',
-  ],
-  tailwindConfig: './packages/frontend/tailwind.config.js',
+  plugins: ['prettier-plugin-prisma', 'prettier-plugin-sql'],
   overrides: [
     {
       files: '*.sql',
