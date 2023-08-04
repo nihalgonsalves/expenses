df1afcc0a97eeb93a4eaf682083a5b4a7c05ac35
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-08-04 19:08:46 +0200

    ux: don't use a loader root for importcsv

--- packages/frontend/src/pages/sheets/SheetImportPage.tsx
+++ packages/frontend/src/pages/sheets/SheetImportPage.tsx
@@ -1,18 +1,15 @@
 import { trpc } from '../../api/trpc';
 import { PersonalExpenseImportStepper } from '../../components/personal-sheets/PersonalExpenseImportStepper';
 import { useParams, PersonalSheetParams } from '../../router';
-import { RootLoader } from '../Root';
+import { Root } from '../Root';
 
 export const SheetImportPage = () => {
   const { sheetId } = useParams(PersonalSheetParams);
-  const result = trpc.sheet.personalSheetById.useQuery(sheetId);
+  const { data } = trpc.sheet.personalSheetById.useQuery(sheetId);
 
   return (
-    <RootLoader
-      result={result}
-      title="Import Expenses"
-      showBackButton
-      render={(sheet) => <PersonalExpenseImportStepper personalSheet={sheet} />}
-    />
+    <Root title="Import Expenses" showBackButton>
+      {data && <PersonalExpenseImportStepper personalSheet={data} />}
+    </Root>
   );
 };
