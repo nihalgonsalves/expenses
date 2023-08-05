290187fbbf931a4c4c03a84f475c92eddf173a3d
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-08-05 02:57:04 +0200

    ux: improve reset cache button (show loading)

--- packages/frontend/src/components/settings/TroubleshootingForm.tsx
+++ packages/frontend/src/components/settings/TroubleshootingForm.tsx
@@ -1,15 +1,28 @@
+import { useState } from 'react';
+
 import { useResetCache } from '../../api/useCacheReset';
 import { useServiceWorkerRegistration } from '../../utils/hooks/useServiceWorkerRegistration';
 import { Button } from '../form/Button';
 
 export const TroubleshootingForm = () => {
+  const [state, setState] = useState<'initial' | 'loading' | 'done'>('initial');
+
   const resetCache = useResetCache();
   const serviceWorker = useServiceWorkerRegistration();
 
   const handleResetCache = async () => {
+    setState('loading');
+
+    // wait at least 1 second
+    const minTimer = new Promise((resolve) => setTimeout(resolve, 1000));
+
     await resetCache();
     await serviceWorker?.unregister();
     localStorage.clear();
+
+    await minTimer;
+    setState('done');
+
     window.location.reload();
   };
 
@@ -22,8 +35,9 @@ export const TroubleshootingForm = () => {
         <Button
           className="btn btn-error btn-outline btn-block mt-4"
           onClick={handleResetCache}
+          isLoading={state === 'loading'}
         >
-          Reset Cache
+          {state === 'done' ? 'Done, reloading...' : 'Reset Cache'}
         </Button>
       </div>
     </div>
