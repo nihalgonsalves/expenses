nihalgonsalves/expenses

commit 7e2d6066f353e0c5f6444d4df9b28b008b96b53f

feat: enable editing personal transactions

packages/backend/src/service/transaction/router.ts
@@ -2,6 +2,7 @@ import { Temporal } from '@js-temporal/polyfill';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
+import type { Money } from '@nihalgonsalves/expenses-shared/money';
 import {
   ZCreateGroupSheetTransactionInput,
   ZCreateSheetTransactionResponse,
@@ -17,9 +18,25 @@ import {
   ZTransactionScheduleListItem,
   type TransactionScheduleListItem,
   ZRecurrenceFrequency,
+  ZTransactionWithSheet,
+  ZUpdatePersonalSheetTransactionInput,
 } from '@nihalgonsalves/expenses-shared/types/transaction';
 
 import { protectedProcedure, router } from '../../trpc';
+
+const mapTransaction = <
+  T extends { amount: number; scale: number; spentAt: Date },
+>(
+  { amount, scale, spentAt, ...transaction }: T,
+  sheet: { currencyCode: string },
+): Omit<T, 'amount' | 'scale' | 'spentAt'> & {
+  spentAt: string;
+  money: Money;
+} => ({
+  ...transaction,
+  spentAt: spentAt.toISOString(),
+  money: { amount, scale, currencyCode: sheet.currencyCode },
+});
 
 export const transactionRouter = router({
   createPersonalSheetTransaction: protectedProcedure
@@ -32,6 +49,22 @@ export const transactionRouter = router({
       );
 
       return ctx.transactionService.createPersonalSheetTransaction(
+        ctx.user,
+        input,
+        sheet,
+      );
+    }),
+
+  updatePersonalSheetTransaction: protectedProcedure
+    .input(ZUpdatePersonalSheetTransactionInput)
+    .output(ZCreateSheetTransactionResponse)
+    .mutation(async ({ input, ctx }) => {
+      const { sheet } = await ctx.sheetService.ensurePersonalSheetMembership(
+        input.personalSheetId,
+        ctx.user.id,
+      );
+
+      return ctx.transactionService.updatePersonalSheetTransaction(
         ctx.user,
         input,
         sheet,
@@ -159,6 +192,38 @@ export const transactionRouter = router({
       );
     }),
 
+  getTransaction: protectedProcedure
+    .input(
+      z.object({
+        sheetId: z.string().min(1),
+        transactionId: z.string().min(1),
+      }),
+    )
+    .output(ZTransactionWithSheet)
+    .query(async ({ input: { sheetId, transactionId }, ctx }) => {
+      const { sheet } = await ctx.sheetService.ensureSheetMembership(
+        sheetId,
+        ctx.user.id,
+      );
+
+      const transaction = await ctx.transactionService.getTransaction(
+        transactionId,
+        sheet,
+      );
+
+      if (!transaction) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Transaction not found',
+        });
+      }
+
+      return {
+        transaction: mapTransaction(transaction, sheet),
+        sheet,
+      };
+    }),
+
   getAllUserTransactions: protectedProcedure
     .input(
       z.object({
@@ -175,12 +240,12 @@ export const transactionRouter = router({
         });
 
       return {
-        expenses: expenses.map(({ spentAt, sheet, ...transaction }) => ({
-          transaction: { ...transaction, spentAt: spentAt.toISOString() },
+        expenses: expenses.map(({ sheet, ...transaction }) => ({
+          transaction: mapTransaction(transaction, sheet),
           sheet,
         })),
-        earnings: earnings.map(({ spentAt, sheet, ...transaction }) => ({
-          transaction: { ...transaction, spentAt: spentAt.toISOString() },
+        earnings: earnings.map(({ sheet, ...transaction }) => ({
+          transaction: mapTransaction(transaction, sheet),
           sheet,
         })),
       };
@@ -207,12 +272,8 @@ export const transactionRouter = router({
         });
 
       return {
-        transactions: transactions.map(
-          ({ amount, scale, spentAt, ...transaction }) => ({
-            ...transaction,
-            spentAt: spentAt.toISOString(),
-            money: { amount, scale, currencyCode: sheet.currencyCode },
-          }),
+        transactions: transactions.map((transaction) =>
+          mapTransaction(transaction, sheet),
         ),
         total,
       };
@@ -283,17 +344,9 @@ export const transactionRouter = router({
 
       return {
         transactions: transactions.map(
-          ({
-            participantBalances,
-            amount,
-            scale,
-            spentAt,
-            ...transaction
-          }) => ({
-            ...transaction,
+          ({ participantBalances, ...transaction }) => ({
+            ...mapTransaction(transaction, sheet),
             participants: participantBalances,
-            spentAt: spentAt.toISOString(),
-            money: { amount, scale, currencyCode: sheet.currencyCode },
             yourBalance: participantBalances.find(
               ({ id }) => id === ctx.user.id,
             )?.balance,

packages/backend/src/service/transaction/service.ts
@@ -28,6 +28,7 @@ import type {
   CreatePersonalSheetTransactionInput,
   GroupSheetParticipantItem,
   CreatePersonalSheetTransactionScheduleInput,
+  UpdatePersonalSheetTransactionInput,
 } from '@nihalgonsalves/expenses-shared/types/transaction';
 import type { User } from '@nihalgonsalves/expenses-shared/types/user';
 
@@ -317,6 +318,53 @@ export class TransactionService {
     });
   }
 
+  async updatePersonalSheetTransaction(
+    user: User,
+    input: Omit<UpdatePersonalSheetTransactionInput, 'personalSheetId'>,
+    personalSheet: Sheet,
+  ) {
+    verifyCurrencies(personalSheet.currencyCode, input.money.currencyCode);
+    verifyAmountIsAbsolute(input.money);
+
+    const transaction = await this.prismaClient.transaction.findUnique({
+      where: { id: input.id },
+    });
+
+    if (!transaction) {
+      throw new TransactionServiceError({
+        code: 'NOT_FOUND',
+        message: 'Transaction not found',
+      });
+    }
+
+    const [, result] = await this.prismaClient.$transaction([
+      this.prismaClient.transaction.delete({
+        where: { id: input.id },
+      }),
+      this.prismaClient.transaction.create({
+        include: {
+          transactionEntries: {
+            include: {
+              user: true,
+            },
+          },
+        },
+        data: {
+          ...mapInputToCreatePersonalTransaction(
+            input,
+            personalSheet,
+            input.id,
+          ),
+          transactionEntries: {
+            create: [mapInputToCreatePersonalTransactionEntry(input, user)],
+          },
+        },
+      }),
+    ]);
+
+    return result;
+  }
+
   async createPersonalSheetTransactionSchedule(
     input: Omit<CreatePersonalSheetTransactionScheduleInput, 'personalSheetId'>,
     personalSheet: Sheet,
@@ -448,7 +496,6 @@ export class TransactionService {
       transaction.transactionEntries,
     );
 
-    // TODO: Background task
     await this.notificationService.sendNotifications(
       Object.fromEntries(
         balances
@@ -504,7 +551,6 @@ export class TransactionService {
       },
     });
 
-    // TODO: Background task
     const messages: Record<string, NotificationPayload> = {};
     if (input.fromId !== user.id)
       messages[input.fromId] = transferToNotificationPayload(
@@ -522,6 +568,15 @@ export class TransactionService {
     await this.notificationService.sendNotifications(messages);
 
     return transaction;
+  }
+
+  async getTransaction(id: string, sheet: Sheet) {
+    return this.prismaClient.transaction.findUnique({
+      where: { id, sheetId: sheet.id },
+      include: {
+        sheet: true,
+      },
+    });
   }
 
   async deleteTransaction(id: string, groupSheet: Sheet) {

packages/backend/src/service/transaction/router.test.ts
@@ -21,7 +21,10 @@ import { generateId } from '../../utils/nanoid';
 const { prisma, useProtectedCaller } = await getTRPCCaller();
 
 describe('createPersonalSheetTransaction', () => {
-  it('creates an transaction', async () => {
+  it.each([
+    ['EXPENSE', -100_00],
+    ['INCOME', 100_00],
+  ] as const)('creates an %s transaction', async (type, amount) => {
     const user = await userFactory(prisma);
     const caller = useProtectedCaller(user);
 
@@ -33,7 +36,7 @@ describe('createPersonalSheetTransaction', () => {
       createPersonalSheetTransactionInput(
         personalSheet.id,
         personalSheet.currencyCode,
-        'EXPENSE',
+        type,
       ),
     );
 
@@ -47,46 +50,11 @@ describe('createPersonalSheetTransaction', () => {
     });
 
     expect(transaction).toMatchObject({
-      type: TransactionType.EXPENSE,
+      type,
     });
 
     expect(transaction?.transactionEntries).toMatchObject([
-      { scale: 2, amount: -100_00, userId: user.id },
-    ]);
-  });
-
-  it('creates an income transaction', async () => {
-    const user = await userFactory(prisma);
-    const caller = useProtectedCaller(user);
-
-    const personalSheet = await personalSheetFactory(prisma, {
-      withOwnerId: user.id,
-    });
-
-    const response = await caller.transaction.createPersonalSheetTransaction(
-      createPersonalSheetTransactionInput(
-        personalSheet.id,
-        personalSheet.currencyCode,
-        'INCOME',
-        100_00,
-      ),
-    );
-
-    expect(response).toMatchObject({
-      id: expect.any(String),
-    });
-
-    const transaction = await prisma.transaction.findUnique({
-      where: { id: response.id },
-      include: { transactionEntries: true },
-    });
-
-    expect(transaction).toMatchObject({
-      type: TransactionType.INCOME,
-    });
-
-    expect(transaction?.transactionEntries).toMatchObject([
-      { scale: 2, amount: 100_00, userId: user.id },
+      { scale: 2, amount, userId: user.id },
     ]);
   });
 
@@ -139,6 +107,141 @@ describe('createPersonalSheetTransaction', () => {
           'EXPENSE',
         ),
       ),
+    ).rejects.toThrow('Sheet not found');
+  });
+});
+
+describe('updatePersonalSheetTransaction', () => {
+  it.each([
+    ['EXPENSE', -50_00],
+    ['INCOME', 50_00],
+  ] as const)('updates an %s transaction', async (type, amount) => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+    });
+
+    const { id } = await caller.transaction.createPersonalSheetTransaction(
+      createPersonalSheetTransactionInput(
+        personalSheet.id,
+        personalSheet.currencyCode,
+        type,
+      ),
+    );
+
+    const response = await caller.transaction.updatePersonalSheetTransaction({
+      id,
+      ...createPersonalSheetTransactionInput(
+        personalSheet.id,
+        personalSheet.currencyCode,
+        type,
+        Math.abs(amount),
+      ),
+    });
+
+    expect(response).toMatchObject({
+      id,
+    });
+
+    const transaction = await prisma.transaction.findUnique({
+      where: { id: response.id },
+      include: { transactionEntries: true },
+    });
+
+    expect(transaction).toMatchObject({
+      type,
+    });
+
+    expect(transaction?.transactionEntries).toMatchObject([
+      { scale: 2, amount, userId: user.id },
+    ]);
+  });
+
+  it("returns 400 if the transaction currency doesn't match", async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+      currencyCode: 'EUR',
+    });
+
+    const { id } = await caller.transaction.createPersonalSheetTransaction(
+      createPersonalSheetTransactionInput(
+        personalSheet.id,
+        personalSheet.currencyCode,
+        'EXPENSE',
+      ),
+    );
+
+    const invalidInput = {
+      id,
+      ...createPersonalSheetTransactionInput(
+        personalSheet.id,
+        'GBP',
+        'EXPENSE',
+      ),
+    };
+
+    await expect(
+      caller.transaction.updatePersonalSheetTransaction(invalidInput),
+    ).rejects.toThrow('Currencies do not match');
+  });
+
+  it('returns 404 if the transaction does not exist', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+      currencyCode: 'EUR',
+    });
+
+    await expect(
+      caller.transaction.updatePersonalSheetTransaction({
+        id: generateId(),
+        ...createPersonalSheetTransactionInput(
+          personalSheet.id,
+          personalSheet.currencyCode,
+          'EXPENSE',
+        ),
+      }),
+    ).rejects.toThrow('Transaction not found');
+  });
+
+  it('returns 404 if the personalSheet does not exist', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    await expect(
+      caller.transaction.updatePersonalSheetTransaction({
+        id: generateId(),
+        ...createPersonalSheetTransactionInput(
+          generateId(),
+          currencyCodeFactory(),
+          'EXPENSE',
+        ),
+      }),
+    ).rejects.toThrow('Sheet not found');
+  });
+
+  it('returns 404 if the user is not the personalSheet owner', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma);
+
+    await expect(
+      caller.transaction.updatePersonalSheetTransaction({
+        id: generateId(),
+        ...createPersonalSheetTransactionInput(
+          personalSheet.id,
+          personalSheet.currencyCode,
+          'EXPENSE',
+        ),
+      }),
     ).rejects.toThrow('Sheet not found');
   });
 });
@@ -298,7 +401,7 @@ describe('batchCreatePersonalSheetTransactions', () => {
     ).rejects.toThrow('Currencies do not match');
   });
 
-  it("returns 400 if the amount isn't abosolute", async () => {
+  it("returns 400 if the amount isn't absolute", async () => {
     const user = await userFactory(prisma);
     const caller = useProtectedCaller(user);
 
@@ -327,7 +430,7 @@ describe('batchCreatePersonalSheetTransactions', () => {
 });
 
 describe('createGroupSheetTransaction', () => {
-  it('creates an transaction', async () => {
+  it('creates an expense transaction', async () => {
     const [user, member] = await Promise.all([
       userFactory(prisma),
       userFactory(prisma),
@@ -935,6 +1038,54 @@ describe('deleteTransactionSchedule', () => {
       }),
     ).rejects.toThrow('Transaction schedule not found');
   });
+});
+
+describe('getTransaction', () => {
+  it('returns a transaction', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+    });
+
+    const { id } = await caller.transaction.createPersonalSheetTransaction(
+      createPersonalSheetTransactionInput(
+        personalSheet.id,
+        personalSheet.currencyCode,
+        'EXPENSE',
+      ),
+    );
+
+    expect(
+      await caller.transaction.getTransaction({
+        sheetId: personalSheet.id,
+        transactionId: id,
+      }),
+    ).toMatchObject({
+      transaction: { id },
+    });
+  });
+
+  it('returns 404 if the transaction does not exist', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+    });
+
+    await expect(
+      caller.transaction.getTransaction({
+        sheetId: personalSheet.id,
+        transactionId: generateId(),
+      }),
+    ).rejects.toThrow('Transaction not found');
+  });
+
+  it.todo('returns 404 if the personalSheet does not exist');
+
+  it.todo('returns 404 if the user is not the personalSheet owner');
 });
 
 describe('getAllUserTransactions', () => {

packages/frontend/src/components/personal-sheets/EditPersonalTransactionForm.tsx
@@ -1,0 +1,153 @@
+import { Temporal } from '@js-temporal/polyfill';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { z } from 'zod';
+
+import type { Sheet } from '@nihalgonsalves/expenses-shared/types/sheet';
+import type { TransactionListItem } from '@nihalgonsalves/expenses-shared/types/transaction';
+
+import { useCurrencyConversion } from '../../api/currencyConversion';
+import { trpc } from '../../api/trpc';
+import { CategoryId } from '../../data/categories';
+import { useNavigatorOnLine } from '../../state/useNavigatorOnLine';
+import { formatCurrency, useMoneyValues } from '../../utils/money';
+import {
+  dateTimeLocalToZonedISOString,
+  isoToTemporalZonedDateTime,
+} from '../../utils/utils';
+import { Button } from '../form/Button';
+import { CategorySelect } from '../form/CategorySelect';
+import { CurrencySelect } from '../form/CurrencySelect';
+import { MoneyField } from '../form/MoneyField';
+import { TextField } from '../form/TextField';
+
+export const EditPersonalTransactionForm = ({
+  transaction,
+  personalSheet,
+}: {
+  transaction: TransactionListItem;
+  personalSheet: Sheet;
+}) => {
+  const navigate = useNavigate();
+  const onLine = useNavigatorOnLine();
+
+  const [amount, setAmount] = useState(Math.abs(transaction.money.amount));
+  const [currencyCode, setCurrencyCode] = useState(personalSheet.currencyCode);
+  const [category, setCategory] = useState<CategoryId | undefined>(
+    z
+      .nativeEnum(CategoryId)
+      .optional()
+      .catch(undefined)
+      .parse(transaction.category),
+  );
+  const [description, setDescription] = useState(transaction.description);
+  const [dateTime, setDateTime] = useState(
+    isoToTemporalZonedDateTime(transaction.spentAt)
+      .toPlainDateTime()
+      .toString(),
+  );
+
+  const [, moneySnapshot] = useMoneyValues(amount, currencyCode);
+
+  const { supportedCurrencies, targetSnapshot: convertedMoneySnapshot } =
+    useCurrencyConversion(
+      Temporal.PlainDate.from(dateTime),
+      currencyCode,
+      personalSheet.currencyCode,
+      moneySnapshot,
+    );
+
+  const utils = trpc.useContext();
+  const { mutateAsync: updatePersonalSheetTransaction, isLoading } =
+    trpc.transaction.updatePersonalSheetTransaction.useMutation();
+
+  const valid = amount > 0;
+
+  const handleCreateTransaction = async () => {
+    const money = convertedMoneySnapshot ?? moneySnapshot;
+
+    await updatePersonalSheetTransaction({
+      id: transaction.id,
+      // this should not be required, but the returned type has an extraneous 'TRANFER' value
+      type: z.enum(['EXPENSE', 'INCOME']).parse(transaction.type),
+      personalSheetId: personalSheet.id,
+      money,
+      category: category ?? CategoryId.Other,
+      description,
+      spentAt: dateTimeLocalToZonedISOString(dateTime),
+    });
+    navigate(`/sheets/${personalSheet.id}/transactions`, { replace: true });
+
+    await Promise.all([
+      utils.transaction.getAllUserTransactions.invalidate(),
+      utils.transaction.getPersonalSheetTransactions.invalidate({
+        personalSheetId: personalSheet.id,
+      }),
+      utils.transaction.getTransaction.invalidate({
+        transactionId: transaction.id,
+      }),
+    ]);
+  };
+
+  const disabled = !valid || !onLine;
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (disabled) return;
+
+        void handleCreateTransaction();
+      }}
+    >
+      <div className="flex items-start gap-4">
+        <MoneyField
+          className="flex-grow"
+          autoFocus
+          label="Amount"
+          bottomLabel={
+            convertedMoneySnapshot
+              ? formatCurrency(convertedMoneySnapshot)
+              : null
+          }
+          currencyCode={currencyCode}
+          amount={amount}
+          setAmount={setAmount}
+        />
+
+        {supportedCurrencies.includes(personalSheet.currencyCode) && (
+          <CurrencySelect
+            options={supportedCurrencies}
+            currencyCode={currencyCode}
+            setCurrencyCode={setCurrencyCode}
+          />
+        )}
+      </div>
+
+      <CategorySelect category={category} setCategory={setCategory} />
+
+      <TextField
+        label="Description"
+        value={description}
+        setValue={setDescription}
+      />
+
+      <TextField
+        label="Date & Time"
+        type="datetime-local"
+        inputClassName="appearance-none"
+        value={dateTime}
+        setValue={setDateTime}
+      />
+
+      <Button
+        className="btn-primary btn-block mt-4"
+        type="submit"
+        disabled={disabled}
+        isLoading={isLoading}
+      >
+        Update
+      </Button>
+    </form>
+  );
+};

packages/frontend/src/components/personal-sheets/PersonalSheetTransactionsExpandedList.tsx
@@ -1,6 +1,8 @@
 import { Temporal } from '@js-temporal/polyfill';
 import { AnimatePresence, motion } from 'framer-motion';
 import { forwardRef, useState } from 'react';
+import { MdEdit } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 import { sumMoneyOrUndefined } from '@nihalgonsalves/expenses-shared/money';
 import type { TransactionListItem } from '@nihalgonsalves/expenses-shared/types/transaction';
@@ -63,6 +65,13 @@ const ExpandedTransactionListItem = forwardRef<
           {expanded && (
             <motion.div className="flex flex-col gap-4 p-0" {...collapse}>
               <div className="divider mb-0" />
+
+              <Link
+                to={`/sheets/${personalSheetId}/transactions/${transaction.id}`}
+                className="btn btn-outline btn-block"
+              >
+                <MdEdit /> Edit
+              </Link>
 
               <TransactionActions
                 sheetId={personalSheetId}

packages/frontend/src/pages/sheets/transactions/EditPersonalSheetTransactionPage.tsx
@@ -1,0 +1,26 @@
+import { trpc } from '../../../api/trpc';
+import { EditPersonalTransactionForm } from '../../../components/personal-sheets/EditPersonalTransactionForm';
+import { TransactionParams, useParams } from '../../../router';
+import { RootLoader } from '../../Root';
+
+export const EditPersonalSheetTransactionPage = () => {
+  const { sheetId, transactionId } = useParams(TransactionParams);
+  const result = trpc.transaction.getTransaction.useQuery({
+    sheetId,
+    transactionId,
+  });
+
+  return (
+    <RootLoader
+      title="Edit Transaction"
+      showBackButton
+      render={({ transaction, sheet }) => (
+        <EditPersonalTransactionForm
+          transaction={transaction}
+          personalSheet={sheet}
+        />
+      )}
+      result={result}
+    />
+  );
+};

commit 63d885fb62fe3fb5fef1a5040e7c06ad5425c222

fix: stats regression from 7e2d6066
getAllUserTransactions should not have used the same mapping
logic as it was already pre-mapped to the right money value.

packages/backend/src/service/transaction/router.ts
@@ -240,12 +240,12 @@ export const transactionRouter = router({
         });
 
       return {
-        expenses: expenses.map(({ sheet, ...transaction }) => ({
-          transaction: mapTransaction(transaction, sheet),
+        expenses: expenses.map(({ sheet, spentAt, ...transaction }) => ({
+          transaction: { ...transaction, spentAt: spentAt.toISOString() },
           sheet,
         })),
-        earnings: earnings.map(({ sheet, ...transaction }) => ({
-          transaction: mapTransaction(transaction, sheet),
+        earnings: earnings.map(({ sheet, spentAt, ...transaction }) => ({
+          transaction: { ...transaction, spentAt: spentAt.toISOString() },
           sheet,
         })),
       };

packages/backend/src/service/transaction/service.ts
@@ -186,31 +186,47 @@ export class TransactionService {
     });
 
     const expenses = data
-      .map(({ transactionEntries, ...transaction }) => ({
-        ...transaction,
-        money: sumTransactions(
-          transaction.sheet.currencyCode,
-          transactionEntries.filter(
-            ({ amount, userId }) =>
-              transaction.type === 'EXPENSE' &&
-              amount < 0 &&
-              userId === user.id,
+      .map(
+        ({
+          transactionEntries,
+          amount: _amount,
+          scale: _scale,
+          ...transaction
+        }) => ({
+          ...transaction,
+          money: sumTransactions(
+            transaction.sheet.currencyCode,
+            transactionEntries.filter(
+              ({ amount, userId }) =>
+                transaction.type === 'EXPENSE' &&
+                amount < 0 &&
+                userId === user.id,
+            ),
           ),
-        ),
-      }))
+        }),
+      )
       .filter(({ money }) => money.amount !== 0);
 
     const earnings = data
-      .map(({ transactionEntries, ...transaction }) => ({
-        ...transaction,
-        money: sumTransactions(
-          transaction.sheet.currencyCode,
-          transactionEntries.filter(
-            ({ amount, userId }) =>
-              transaction.type === 'INCOME' && amount > 0 && userId === user.id,
+      .map(
+        ({
+          transactionEntries,
+          amount: _amount,
+          scale: _scale,
+          ...transaction
+        }) => ({
+          ...transaction,
+          money: sumTransactions(
+            transaction.sheet.currencyCode,
+            transactionEntries.filter(
+              ({ amount, userId }) =>
+                transaction.type === 'INCOME' &&
+                amount > 0 &&
+                userId === user.id,
+            ),
           ),
-        ),
-      }))
+        }),
+      )
       .filter(({ money }) => money.amount !== 0);
 
     return {

packages/backend/src/service/transaction/router.test.ts
@@ -1,3 +1,4 @@
+import { Temporal } from '@js-temporal/polyfill';
 import { TransactionType } from '@prisma/client';
 import { describe, expect, it } from 'vitest';
 
@@ -1095,7 +1096,113 @@ describe('getTransaction', () => {
 });
 
 describe('getAllUserTransactions', () => {
-  it.todo('returns transactions from all sheets');
+  it('returns transactions from all sheets', async () => {
+    const user = await userFactory(prisma);
+    const caller = useProtectedCaller(user);
+
+    const member = await userFactory(prisma);
+
+    const personalSheet = await personalSheetFactory(prisma, {
+      withOwnerId: user.id,
+    });
+
+    const groupSheet = await groupSheetFactory(prisma, {
+      withOwnerId: user.id,
+      withParticipantIds: [member.id],
+    });
+
+    await Promise.all(
+      (['EXPENSE', 'INCOME'] as const).map(async (type) => {
+        await caller.transaction.createPersonalSheetTransaction(
+          createPersonalSheetTransactionInput(
+            personalSheet.id,
+            personalSheet.currencyCode,
+            type,
+          ),
+        );
+
+        await caller.transaction.createGroupSheetTransaction(
+          createGroupSheetTransactionInput(
+            type,
+            groupSheet.id,
+            groupSheet.currencyCode,
+            user.id,
+            member.id,
+          ),
+        );
+      }),
+    );
+
+    expect(
+      await caller.transaction.getAllUserTransactions({
+        fromTimestamp: Temporal.Now.instant()
+          .subtract({ minutes: 1 })
+          .toString(),
+        toTimestamp: Temporal.Now.instant().add({ minutes: 1 }).toString(),
+      }),
+    ).toMatchObject({
+      earnings: [
+        {
+          sheet: {
+            type: 'GROUP',
+          },
+          transaction: {
+            type: 'INCOME',
+            category: 'other',
+            description: 'test group income',
+            money: {
+              amount: 2500,
+              scale: 2,
+            },
+          },
+        },
+        {
+          sheet: {
+            type: 'PERSONAL',
+          },
+          transaction: {
+            type: 'INCOME',
+            category: 'other',
+            description: 'test personal income',
+            money: {
+              amount: 10000,
+              scale: 2,
+            },
+          },
+        },
+      ],
+      expenses: [
+        {
+          sheet: {
+            type: 'GROUP',
+          },
+          transaction: {
+            type: 'EXPENSE',
+            category: 'other',
+            description: 'test group expense',
+            money: {
+              amount: -2500,
+              scale: 2,
+            },
+          },
+        },
+        {
+          sheet: {
+            type: 'PERSONAL',
+          },
+          transaction: {
+            type: 'EXPENSE',
+            category: 'other',
+            description: 'test personal expense',
+            money: {
+              amount: -10000,
+              scale: 2,
+            },
+          },
+        },
+      ],
+    });
+  });
 });
 
 describe('getPersonalSheetTransactions', () => {