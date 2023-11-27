e5529dc510b81272a4bee577b4976235c37489fe
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-11-27 13:44:31 +0100

    chore: add full dev container

---
+++ packages/backend/bin/prepare-local-env.mjs
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+// @ts-check
+
+import { writeFile } from 'fs/promises';
+
+import { default as webPush } from 'web-push';
+
+const { publicKey, privateKey } = webPush.generateVAPIDKeys();
+
+await writeFile(
+  new URL('../.env', import.meta.url),
+  Object.entries({ VAPID_PRIVATE_KEY: privateKey, VAPID_PUBLIC_KEY: publicKey })
+    .map(([key, value]) => `${key}=${value}`)
+    .join('\n'),
+  'utf8',
+);
