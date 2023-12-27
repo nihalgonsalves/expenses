2c4d8863aade19b54b494c3881981daff2de6996
Nihal Gonsalves <nihal@nihalgonsalves.com>
2023-12-27 18:17:38 +0100

    fix: page title test

--- packages/e2e/tests/example.spec.ts
+++ packages/e2e/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('/');
+  await page.goto('/settings');
 
-  await expect(page).toHaveTitle(/expenses/);
+  await expect(page).toHaveTitle(/Settings/);
 });
