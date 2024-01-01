377ffb802c5243a1fdc65ff913fe477b2476bbb0
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-01-01 09:47:32 +0100

    ci: add codecov

--- packages/shared/vitest.config.ts
+++ packages/shared/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: { enabled: true, reporter: ['html'] },
     exclude: ['**/build/**/*'],
   },
 });
