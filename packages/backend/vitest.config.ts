431213190b944d60f899af1441788a2d4bc17ee2
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-01-01 10:36:07 +0100

    ci: add codecov (#79)

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    coverage: { enabled: true, reporter: ['html'] },
     setupFiles: fileURLToPath(new URL('./test/setup.ts', import.meta.url)),
     exclude: ['**/build/**/*', '**/dist/**/*'],
   },
