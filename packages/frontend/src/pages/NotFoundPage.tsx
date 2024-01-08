ee9f778f4214a10c6dddb583507d4a0744367ecb
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-01-08 20:29:01 +0100

    ui: improve not found page

--- packages/frontend/src/pages/NotFoundPage.tsx
+++ packages/frontend/src/pages/NotFoundPage.tsx
@@ -1,5 +1,10 @@
 import { Root } from './Root';
 
 export const NotFoundPage = () => (
-  <Root title="Not Found">Nothing to see here</Root>
+  <Root
+    title="Not Found"
+    className="bg-muted text-muted-foreground grid place-items-center text-center"
+  >
+    Not Found
+  </Root>
 );
