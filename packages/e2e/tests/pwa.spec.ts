nihalgonsalves/expenses

commit 036aa5811006997bc95358c344e9c30d3f1481f6

refactor: use honojs

packages/backend/src/pwaRouter.ts
@@ -1,4 +1,4 @@
-import type { FastifyInstance } from 'fastify';
+import { Hono } from 'hono';
 
 import {
   ZTheme,
@@ -12,52 +12,59 @@ import type { makeCreateContext } from './context';
  * Server-side router that serves a themed manifest for the PWA
  */
 
-export const makePWARouter =
-  (createContext: ReturnType<typeof makeCreateContext>) =>
-  async (fastify: FastifyInstance, _options: unknown) => {
-    fastify.get('/manifest.webmanifest', async (request, reply) => {
-      const context = await createContext({ req: request, res: reply });
+export const makePWARouter = (
+  createContext: ReturnType<typeof makeCreateContext>,
+) => {
+  const app = new Hono();
 
-      const theme = ZTheme.catch(THEME_DEFAULT).parse(context.user?.theme);
-      const { primary } = themeColors[theme];
-
-      await reply.send({
-        id: '/',
-        name: 'Expenses',
-        short_name: 'Expenses',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#ffffff',
-        lang: 'en',
-        scope: '/',
-        description: 'Expenses App',
-        theme_color: primary,
-        icons: [
-          ...['any', 'maskable'].map((purpose) => ({
-            src: `/assets/icon-normal-${theme}.svg`,
-            type: 'image/svg+xml',
-            sizes: 'any 512x512',
-            purpose,
-          })),
-          {
-            src: `/assets/icon-maskable-${theme}.svg`,
-            type: 'image/svg+xml',
-            sizes: 'any 512x512 192x192 180x180 120x120',
-            purpose: 'maskable',
-          },
-          {
-            src: `/assets/icon-${theme}.png`,
-            type: 'image/png',
-            sizes: '180x180',
-            purpose: 'any',
-          },
-          {
-            src: '/assets/icon-monochrome.svg',
-            type: 'image/svg+xml',
-            sizes: '16x16',
-            purpose: 'monochrome',
-          },
-        ],
-      });
+  app.get('/manifest.webmanifest', async (c) => {
+    const context = await createContext({
+      req: c.req.raw,
+      resHeaders: c.res.headers,
     });
-  };
+
+    const theme = ZTheme.catch(THEME_DEFAULT).parse(context.user?.theme);
+    const { primary } = themeColors[theme];
+
+    c.json({
+      id: '/',
+      name: 'Expenses',
+      short_name: 'Expenses',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      lang: 'en',
+      scope: '/',
+      description: 'Expenses App',
+      theme_color: primary,
+      icons: [
+        ...['any', 'maskable'].map((purpose) => ({
+          src: `/assets/icon-normal-${theme}.svg`,
+          type: 'image/svg+xml',
+          sizes: 'any 512x512',
+          purpose,
+        })),
+        {
+          src: `/assets/icon-maskable-${theme}.svg`,
+          type: 'image/svg+xml',
+          sizes: 'any 512x512 192x192 180x180 120x120',
+          purpose: 'maskable',
+        },
+        {
+          src: `/assets/icon-${theme}.png`,
+          type: 'image/png',
+          sizes: '180x180',
+          purpose: 'any',
+        },
+        {
+          src: '/assets/icon-monochrome.svg',
+          type: 'image/svg+xml',
+          sizes: '16x16',
+          purpose: 'monochrome',
+        },
+      ],
+    });
+  });
+
+  return app;
+};

packages/backend/src/BullBoardHonoAdapter.ts
@@ -1,0 +1,202 @@
+import path from 'path';
+
+import type {
+  AppControllerRoute,
+  AppViewRoute,
+  BullBoardQueues,
+  ControllerHandlerReturnType,
+  IServerAdapter,
+  UIConfig,
+} from '@bull-board/api/dist/typings/app';
+import type { serveStatic as nodeServeStatic } from '@hono/node-server/serve-static';
+import ejs from 'ejs';
+import { Hono } from 'hono';
+import type { Context } from 'hono';
+import type { serveStatic as bunServeStatic } from 'hono/bun';
+import type { serveStatic as cloudflarePagesServeStatic } from 'hono/cloudflare-pages';
+import type { serveStatic as cloudflareWorkersServeStatic } from 'hono/cloudflare-workers';
+import type { serveStatic as denoServeStatic } from 'hono/deno';
+
+export class BullBoardHonoAdapter implements IServerAdapter {
+  protected bullBoardQueues: BullBoardQueues | undefined;
+
+  protected errorHandler:
+    | ((error: Error) => ControllerHandlerReturnType)
+    | undefined;
+
+  protected uiConfig?: UIConfig;
+
+  protected staticRoute?: string;
+
+  protected staticPath?: string;
+
+  protected entryRoute?: AppViewRoute;
+
+  protected viewPath?: string;
+
+  protected apiRoutes: Hono;
+
+  constructor(
+    protected serveStatic:
+      | typeof bunServeStatic
+      | typeof nodeServeStatic
+      | typeof cloudflarePagesServeStatic
+      | typeof cloudflareWorkersServeStatic
+      | typeof denoServeStatic,
+  ) {
+    this.apiRoutes = new Hono();
+  }
+
+  setStaticPath(staticRoute: string, staticPath: string): this {
+    this.staticRoute = staticRoute;
+    this.staticPath = staticPath;
+    return this;
+  }
+
+  setViewsPath(viewPath: string): this {
+    this.viewPath = viewPath;
+    return this;
+  }
+
+  setErrorHandler(
+    handler: (error: Error) => ControllerHandlerReturnType,
+  ): this {
+    this.errorHandler = handler;
+    return this;
+  }
+
+  setApiRoutes(routes: readonly AppControllerRoute[]): this {
+    const { errorHandler, bullBoardQueues } = this;
+
+    if (!errorHandler || !bullBoardQueues) {
+      throw new Error('');
+    }
+
+    routes.forEach(({ method: methodOrMethods, route, handler }) => {
+      const methods = Array.isArray(methodOrMethods)
+        ? methodOrMethods
+        : [methodOrMethods];
+
+      methods.forEach((m) => {
+        this.registerRoute(route, m, handler);
+      });
+    });
+
+    return this;
+  }
+
+  private registerRoute(
+    routeOrRoutes: string | string[],
+    method: 'get' | 'post' | 'put',
+    handler: AppControllerRoute['handler'],
+  ) {
+    const { bullBoardQueues } = this;
+
+    if (!bullBoardQueues) {
+      throw new Error('');
+    }
+
+    const routes = Array.isArray(routeOrRoutes)
+      ? routeOrRoutes
+      : [routeOrRoutes];
+
+    routes.forEach((route) => {
+      this.apiRoutes[method](route, async (c: Context) => {
+        try {
+          const response = await handler({
+            queues: bullBoardQueues,
+            params: c.req.param(),
+            query: c.req.query(),
+          });
+          return c.json(response.body, response.status || 200);
+        } catch (e) {
+          if (!this.errorHandler || !(e instanceof Error)) {
+            throw e;
+          }
+
+          const response = this.errorHandler(e);
+
+          if (typeof response.body === 'string') {
+            return c.text(response.body, response.status);
+          }
+
+          return c.json(response.body, response.status);
+        }
+      });
+    });
+  }
+
+  setEntryRoute(routeDef: AppViewRoute): this {
+    this.entryRoute = routeDef;
+    return this;
+  }
+
+  setQueues(bullBoardQueues: BullBoardQueues): this {
+    this.bullBoardQueues = bullBoardQueues;
+    return this;
+  }
+
+  setUIConfig(config: UIConfig): this {
+    this.uiConfig = config;
+    return this;
+  }
+
+  registerPlugin(basePath: string) {
+    const {
+      staticRoute,
+      staticPath,
+      entryRoute,
+      viewPath,
+      bullBoardQueues,
+      uiConfig,
+    } = this;
+
+    if (
+      !staticRoute ||
+      !staticPath ||
+      !entryRoute ||
+      !viewPath ||
+      !bullBoardQueues ||
+      !uiConfig
+    ) {
+      throw new Error(
+        `Please call 'setErrorHandler' before using 'registerPlugin'`,
+      );
+    }
+
+    const app = new Hono();
+
+    app.get(
+      `${this.staticRoute}/*`,
+      this.serveStatic({
+        root: path.relative(process.cwd(), staticPath),
+        rewriteRequestPath: (p: string) =>
+          p.replace(path.join(basePath, staticRoute), ''),
+      }),
+    );
+
+    app.route('/', this.apiRoutes);
+
+    const routeOrRoutes = entryRoute.route;
+    const routes = Array.isArray(routeOrRoutes)
+      ? routeOrRoutes
+      : [routeOrRoutes];
+
+    routes.forEach((route) => {
+      app[entryRoute.method](route, async (c: Context) => {
+        const { name: fileName, params } = entryRoute.handler({
+          basePath,
+          uiConfig,
+        });
+
+        const template = await ejs.renderFile(
+          `${this.viewPath}/${fileName}`,
+          params,
+        );
+        return c.html(template);
+      });
+    });
+
+    return app;
+  }
+}

packages/backend/src/app.ts
@@ -1,79 +1,81 @@
 import { createBullBoard } from '@bull-board/api';
 import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
-import { FastifyAdapter } from '@bull-board/fastify';
+import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { trpcServer } from '@hono/trpc-server';
 import { PrismaClient } from '@prisma/client';
-import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
-import fastify from 'fastify';
+import { Hono } from 'hono';
+import { showRoutes } from 'hono/dev';
 import IORedis from 'ioredis';
 
+import { BullBoardHonoAdapter } from './BullBoardHonoAdapter';
 import { config } from './config';
 import { makeCreateContext } from './context';
 import { makePWARouter } from './pwaRouter';
 import { appRouter } from './router';
 import { startWorkers } from './startWorkers';
 
-const server = fastify({
-  maxParamLength: 5000,
-  logger: true,
-});
+export const createApp = async (prisma: PrismaClient, redis: IORedis) => {
+  const app = new Hono();
+  const workers = await startWorkers(prisma, redis);
+
+  const createContext = makeCreateContext(prisma, workers);
+
+  app.use(
+    '/trpc/*',
+    trpcServer({
+      router: appRouter,
+      createContext,
+    }),
+  );
+
+  app.route('/', makePWARouter(createContext));
+
+  if (config.ENABLE_ADMIN) {
+    const serverAdapter = new BullBoardHonoAdapter(serveStatic);
+
+    createBullBoard({
+      queues: Object.values(workers).map(
+        ({ queue }) => new BullMQAdapter(queue),
+      ),
+      serverAdapter,
+    });
+
+    app.route('/admin/queue', serverAdapter.registerPlugin('/admin/queue'));
+  }
+
+  return app;
+};
 
 void (async () => {
   const prisma = new PrismaClient();
   const redis = new IORedis(config.REDIS_URL, { maxRetriesPerRequest: null });
 
   try {
-    const workers = await startWorkers(prisma, redis);
+    const app = await createApp(prisma, redis);
 
-    const createContext = makeCreateContext(prisma, workers);
-
-    await server.register(fastifyTRPCPlugin, {
-      prefix: '/trpc',
-      trpcOptions: {
-        router: appRouter,
-        createContext,
-      },
-    });
-
-    await server.register(makePWARouter(createContext));
-
-    if (config.ENABLE_ADMIN) {
-      const serverAdapter = new FastifyAdapter();
-
-      createBullBoard({
-        queues: Object.values(workers).map(
-          ({ queue }) => new BullMQAdapter(queue),
-        ),
-        serverAdapter,
-      });
-
-      serverAdapter.setBasePath('/admin/queue');
-      await server.register(serverAdapter.registerPlugin(), {
-        basePath: '/admin/queue',
-        prefix: '/admin/queue',
-      });
+    if (!IS_PROD) {
+      showRoutes(app);
     }
 
-    const address = await server.listen({
-      host: config.LISTEN_HOST,
-      port: config.PORT,
-    });
+    const server = serve(
+      {
+        fetch: app.fetch,
+        hostname: config.LISTEN_HOST,
+        port: config.PORT,
+      },
+      ({ address, port }) => {
+        console.log(`Server listening at http://${address}:${port}`);
+      },
+    );
 
     process.on('SIGINT', () => {
       console.log(`SIGINT received, shutting web server down`);
 
-      void server
-        .close()
-        .then(() => {
-          process.exit();
-        })
-        .catch(() => {
-          process.exit(1);
-        });
+      server.close();
     });
-
-    console.log(`Server running at ${address}`);
   } catch (e) {
-    server.log.error(e);
+    console.error(e);
     process.exit(1);
   }
 })();

packages/backend/src/context.ts
@@ -1,5 +1,5 @@
 import type { PrismaClient } from '@prisma/client';
-import type { CreateFastifyContextOptions } from '@trpc/server/adapters/fastify';
+import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch';
 import cookie from 'cookie';
 import { UAParser } from 'ua-parser-js';
 
@@ -20,7 +20,7 @@ import type { Workers } from './startWorkers';
 export const AUTH_COOKIE_NAME = 'auth';
 
 export const getMaybeUser = async (
-  cookieHeader: string | undefined,
+  cookieHeader: string | null,
   setJwtToken: (value: JWTToken | null) => Promise<void>,
   userServiceImpl: Pick<UserService, 'exchangeToken'>,
 ) => {
@@ -69,14 +69,14 @@ export const makeCreateContext = (prisma: PrismaClient, workers: Workers) => {
     config.FRANKFURTER_BASE_URL,
   );
 
-  return async ({ req, res }: CreateFastifyContextOptions) => {
+  return async ({ req, resHeaders }: FetchCreateContextFnOptions) => {
     const setJwtToken = async (value: JWTToken | null) => {
       if (!value) {
-        void res.header('clear-site-data', '"*"');
+        resHeaders.set('clear-site-data', '"*"');
       }
 
       // Clear old versions of the cookie
-      void res.header(
+      resHeaders.append(
         'Set-Cookie',
         cookie.serialize(AUTH_COOKIE_NAME, value ?? '', {
           path: '/api/trpc',
@@ -86,7 +86,7 @@ export const makeCreateContext = (prisma: PrismaClient, workers: Workers) => {
         }),
       );
 
-      void res.header(
+      resHeaders.append(
         'Set-Cookie',
         cookie.serialize(AUTH_COOKIE_NAME, value ?? '', {
           path: '/',
@@ -100,9 +100,15 @@ export const makeCreateContext = (prisma: PrismaClient, workers: Workers) => {
 
     return {
       prisma,
-      user: await getMaybeUser(req.headers.cookie, setJwtToken, userService),
+      user: await getMaybeUser(
+        req.headers.get('cookie'),
+        setJwtToken,
+        userService,
+      ),
       get userAgent() {
-        return new UAParser(req.headers['user-agent']).getResult();
+        return new UAParser(
+          req.headers.get('user-agent') ?? undefined,
+        ).getResult();
       },
       userService,
       sheetService,

commit dbfb903cf0888012c8384d4ca7ccf07d6c5ca434

fix: serve manifest correctly
regression from 036aa581

packages/backend/src/pwaRouter.ts
@@ -6,6 +6,7 @@ import {
   themeColors,
 } from "@nihalgonsalves/expenses-shared/types/theme";
 
+import { config } from "./config";
 import type { makeCreateContext } from "./context";
 
 /**
@@ -26,44 +27,50 @@ export const makePWARouter = (
     const theme = ZTheme.catch(THEME_DEFAULT).parse(context.user?.theme);
     const { primary } = themeColors[theme];
 
-    c.json({
-      id: "/",
-      name: "Expenses",
-      short_name: "Expenses",
-      start_url: "/",
-      display: "standalone",
-      background_color: "#ffffff",
-      lang: "en",
-      scope: "/",
-      description: "Expenses App",
-      theme_color: primary,
-      icons: [
-        ...["any", "maskable"].map((purpose) => ({
-          src: `/assets/icon-normal-${theme}.svg`,
-          type: "image/svg+xml",
-          sizes: "any 512x512",
-          purpose,
-        })),
-        {
-          src: `/assets/icon-maskable-${theme}.svg`,
-          type: "image/svg+xml",
-          sizes: "any 512x512 192x192 180x180 120x120",
-          purpose: "maskable",
-        },
-        {
-          src: `/assets/icon-${theme}.png`,
-          type: "image/png",
-          sizes: "180x180",
-          purpose: "any",
-        },
-        {
-          src: "/assets/icon-monochrome.svg",
-          type: "image/svg+xml",
-          sizes: "16x16",
-          purpose: "monochrome",
-        },
-      ],
-    });
+    return c.json(
+      {
+        id: "/",
+        name: config.APP_NAME,
+        short_name: config.APP_NAME,
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        lang: "en",
+        scope: "/",
+        description: "Expenses App",
+        theme_color: primary,
+        icons: [
+          ...["any", "maskable"].map((purpose) => ({
+            src: `/assets/icon-normal-${theme}.svg`,
+            type: "image/svg+xml",
+            sizes: "any 512x512",
+            purpose,
+          })),
+          {
+            src: `/assets/icon-maskable-${theme}.svg`,
+            type: "image/svg+xml",
+            sizes: "any 512x512 192x192 180x180 120x120",
+            purpose: "maskable",
+          },
+          {
+            src: `/assets/icon-${theme}.png`,
+            type: "image/png",
+            sizes: "180x180",
+            purpose: "any",
+          },
+          {
+            src: "/assets/icon-monochrome.svg",
+            type: "image/svg+xml",
+            sizes: "16x16",
+            purpose: "monochrome",
+          },
+        ],
+      },
+      200,
+      {
+        "content-type": "application/manifest+json",
+      },
+    );
   });
 
   return app;

packages/e2e/tests/pwa.spec.ts
@@ -1,0 +1,18 @@
+import { test, expect } from "../utils/test";
+
+test("web manifest loads", async ({ page, request }) => {
+  await page.goto("/");
+
+  const manifestHref = await page
+    .locator("link[rel=manifest]")
+    .getAttribute("href");
+
+  expect(manifestHref).toBe("/api/manifest.webmanifest");
+
+  const manifest = await request.get(manifestHref!);
+
+  expect(manifest.status()).toBe(200);
+  expect(manifest.headers()).toMatchObject({
+    "content-type": "application/manifest+json",
+  });
+});